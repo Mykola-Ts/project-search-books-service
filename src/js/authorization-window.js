04f80659fe06a76da5c96556aed292517c1eeb0b
groguls <groguls@gmail.com>
2023-08-07 22:22:54 +0200

    fixed haeder buttons, services code optimization

--- src/js/authorization-window.js
+++ src/js/authorization-window.js
@@ -1,23 +1,6 @@
 // AUTHORIZATION WINDOW
 
-import { initializeApp } from 'firebase/app';
-import 'firebase/auth';
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-} from 'firebase/auth';
-import {
-  getFirestore,
-  collection,
-  doc,
-  setDoc,
-  getDoc,
-  addDoc,
-  getDocs,
-} from 'firebase/firestore';
+import FirebaseService from './firebase-services';
 
 const refs = {
   logInBtn: document.querySelector('button[data-login-button]'),
@@ -31,24 +14,27 @@ const refs = {
   input: document.querySelector('#name'),
 };
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD73oYitNm6HMY6U12Qvku2isqF0ZeRwg0',
-  authDomain: 'search-book-service.firebaseapp.com',
-  projectId: 'search-book-service',
-  storageBucket: 'search-book-service.appspot.com',
-  messagingSenderId: '941650109651',
-  appId: '1:941650109651:web:2653c587a2d01f772fa026',
-};
+// const selectors = {
+//   logInBtn: 'button[data-login-button]',
+//   logOutBtn: 'button[data-logout-button]',
+//   authBtn: 'button[data-modal-signup]',
+//   signUpLink: document.querySelector('button[data-modal-signup-link]'),
+//   signInLink: document.querySelector('button[data-modal-signin-link]'),
+//   modal: document.querySelector('div[data-modal]'),
+//   closeBtn: document.querySelector('button[data-modal-close]'),
+//   form: document.querySelector('.auth-form'),
+//   input: document.querySelector('#name'),
+// }
 
-const LOCALSTORAGE_KEY = 'currentUser';
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const db = getFirestore();
+const firebaseService = new FirebaseService(refs);
 
-refs.logInBtn.addEventListener('click', monitorAuthState);
-refs.logOutBtn.addEventListener('click', onSignOut);
-refs.form.addEventListener('submit', onAuthFormSubmit);
-refs.closeBtn.addEventListener('click', closeAuthModal);
+firebaseService.logInBtn.addEventListener(
+  'click',
+  firebaseService.monitorAuthState
+);
+firebaseService.logOutBtn.addEventListener('click', firebaseService.onSignOut);
+firebaseService.modal.addEventListener('submit', onAuthFormSubmit);
+refs.closeBtn.addEventListener('click', firebaseService.closeAuthModal);
 refs.signInLink.addEventListener('click', onSignInLink);
 refs.signUpLink.addEventListener('click', onSignUpLink);
 
@@ -61,90 +47,20 @@ function onAuthFormSubmit(evt) {
       email: { value: email },
       password: { value: password },
     },
-  } = evt.currentTarget;
+  } = evt.target;
 
   name = name || email;
 
   switch (evt.submitter.innerText) {
     case 'SIGN UP':
-      evt.currentTarget.reset();
-      createUser(email, password, name);
+      evt.target.reset();
+      firebaseService.createUser(email, password, name);
       break;
     case 'SIGN IN':
-      evt.currentTarget.reset();
-      signInUser(email, password, name);
+      evt.target.reset();
+      firebaseService.signInUser(email, password, name);
       break;
   }
-  const { currentUser } = firebaseApp.auth();
-  console.log(currentUser.uid);
-}
-
-function monitorAuthState() {
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      refs.logOutBtn.hidden = refs.logOutBtn.hidden ? false : true;
-    } else {
-      removeFromLocalStorage(LOCALSTORAGE_KEY);
-      showAuthModal();
-    }
-  });
-}
-
-async function createUser(email, password, name) {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const uid = userCredential.user.uid;
-    refs.logInBtn.textContent = name;
-    const userData = { uid, name, email };
-    saveToLocalStorage(LOCALSTORAGE_KEY, userData);
-    closeAuthModal();
-    await setDoc(doc(db, `users/${uid}`), userData, { merge: true });
-  } catch (error) {
-    console.log(error.code);
-    console.log(error.message);
-  }
-}
-
-async function signInUser(email, password) {
-  try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const uid = userCredential.user.uid;
-    const docSnap = await getDoc(doc(db, `users/${uid}`));
-    saveToLocalStorage(LOCALSTORAGE_KEY, docSnap.data());
-    refs.logInBtn.textContent = docSnap.data().name;
-    closeAuthModal();
-  } catch (error) {
-    console.log(error.code);
-    console.log(error.message);
-  }
-}
-
-async function onSignOut() {
-  try {
-    await signOut(auth);
-    refs.logOutBtn.hidden = true;
-    refs.logInBtn.textContent = 'Sign Up';
-    closeAuthModal();
-  } catch (error) {
-    console.log(error.code);
-    console.log(error.message);
-  }
-}
-
-function showAuthModal() {
-  refs.modal.classList.remove('is-hidden');
-}
-
-function closeAuthModal() {
-  refs.modal.classList.add('is-hidden');
 }
 
 function onSignUpLink() {
@@ -155,60 +71,4 @@ function onSignUpLink() {
 function onSignInLink() {
   refs.input.hidden = true;
   refs.authBtn.textContent = 'Sign In';
-}
-
-const userCollectionRef = collection(db, 'users');
-
-async function addData(data) {
-  const docData = loadFromLocalStorage(LOCALSTORAGE_KEY);
-  console.log(docData);
-  try {
-    await setDoc(doc(db, `users/${docData.uid}`), docData, { merge: true });
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
-}
-
-async function addNewDocument(data) {
-  try {
-    const newDoc = await addDoc(doc(db, `users/${docData.uid}`), docData, {
-      merge: true,
-    });
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
-}
-
-async function readData(params) {
-  const querySnapshot = await getDocs(collection(db, 'users'));
-  querySnapshot.forEach(doc => {
-    console.log(`${doc.id} => ${doc.data()}`);
-    console.log(JSON.stringify(doc.data()));
-  });
-}
-
-function saveToLocalStorage(key, value) {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (error) {
-    console.error('Set state error: ', error.message);
-  }
-}
-
-function loadFromLocalStorage(key) {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
-}
-
-function removeFromLocalStorage(key) {
-  try {
-    localStorage.removeItem(key);
-  } catch (error) {
-    console.error('Remove state error: ', error.message);
-  }
 }
--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,39 +1,43 @@
 // SHOPPING LIST
 const shoppinglist = document.querySelector(`.shopping-list`);
 const btnFromHeader = document.querySelector(`.btn-to-list`);
-const blockSupportUkraine = document.querySelector(`.shopping-list-support-ukraine`);
-const shoppinglistContainer = document.querySelector(`.shopping-list-container`);
+const blockSupportUkraine = document.querySelector(
+  `.shopping-list-support-ukraine`
+);
+const shoppinglistContainer = document.querySelector(
+  `.shopping-list-container`
+);
 
 // Для тестирования - получение и загрузка данных в localStorage.
 // После загрузки данных , нужно закоментировать.
 function getBooks() {
-    return fetch(`https://books-backend.p.goit.global/books/category?category=Series Books`)
-        .then((response) => {
-        if (!response.ok) {
-        throw new Error(response.statusText)
-            }
-            const dataBook = response.json();
-        return dataBook;
-        })
-};
+  return fetch(
+    `https://books-backend.p.goit.global/books/category?category=Series Books`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const dataBook = response.json();
+    return dataBook;
+  });
+}
 getBooks();
 function addToLocalstorage() {
-    getBooks().then(dataBook => {
-        let book = dataBook;
-        dataChangeLocalstorage(`project`, book);
-    })
+  getBooks().then(dataBook => {
+    let book = dataBook;
+    dataChangeLocalstorage(`project`, book);
+  });
 }
 addToLocalstorage();
 
 function dataChangeLocalstorage(key, value) {
-    try {
+  try {
     const serializedState = JSON.stringify(value);
     localStorage.setItem(key, serializedState);
-    } catch (error) {
-    console.error("Set state error: ", error.message);
-    }
-};
-
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+}
 
 // localStorage.clear();
 
@@ -44,30 +48,29 @@ function dataChangeLocalstorage(key, value) {
 btnFromHeader.addEventListener(`click`, onCreatePageShoppingList);
 
 function onCreatePageShoppingList(e) {
-    e.preventDefault();
-    let data = getDataLocalStorage();
-    createMarkup(data);
+  e.preventDefault();
+  let data = getDataLocalStorage();
+  createMarkup(data);
 }
 
-
 function getDataLocalStorage() {
-    const savedData = localStorage.getItem(`project`);
-    let parsedData = [];
-    return parsedData = JSON.parse(savedData);
-};
-
+  const savedData = localStorage.getItem(`project`);
+  let parsedData = [];
+  return (parsedData = JSON.parse(savedData));
+}
 
 function createMarkup(data) {
-
-    // blockSupportUkraine.innerHTML = '<div class="shopping-list-support-ukraine"> Support UKraine</div>'
-    shoppinglistContainer.innerHTML = `
+  // blockSupportUkraine.innerHTML = '<div class="shopping-list-support-ukraine"> Support UKraine</div>'
+  shoppinglistContainer.innerHTML = `
     <h2 class="shopping-list-title-part1 ">Shopping <span class="shopping-list-title-part2">List</span></h2>
-    `
-    // let data = getData();
-    // console.log(data);
+    `;
+  // let data = getData();
+  // console.log(data);
 
-    if (data.length != 0) {
-        shoppinglistContainer.innerHTML += data.map(el => `
+  if (data.length != 0) {
+    shoppinglistContainer.innerHTML += data
+      .map(
+        el => `
     
     <div class="shopping-list-card" data-title="${el.title}">
     
@@ -114,55 +117,51 @@ function createMarkup(data) {
         </svg>
     </button>
     </div>
-    `).join(``)
-    }
-    else {
-        shoppinglistContainer.innerHTML += `
+    `
+      )
+      .join(``);
+  } else {
+    shoppinglistContainer.innerHTML += `
         <div class="empty-shopping-list">
         <p class="shopping-list-text-empty">This page is empty, add some books and proceed to order.</p>
         <img class="shopping-list-empty-img" src="./img/empty-shopping-list.png" alt="empty list " width="265"
         </div>
-        `
-    }
-    }
-    
+        `;
+  }
+}
 
 // doMarkup();
 
 const btnDeletebook = document.querySelector(`.shopping-list`);
 btnDeletebook.addEventListener(`click`, onDeleteBook);
 
-
 function onDeleteBook(e) {
-    e.preventDefault();
-    console.log(e.target);
-    
-    if (!e.target.classList.contains(`icon-delete-button`)) {
-        // console.log(`no delete`);
-        return;
-    }
-    // console.log(` delete`);
-    const deleteBook = e.target.closest(`.shopping-list-card`);
-    // console.log(deleteBook);
-    const deleteBookName = deleteBook.dataset.title;
-    // console.log(deleteBookName);
-    let data = getDataLocalStorage();
-    // console.log(data);
+  e.preventDefault();
+  console.log(e.target);
 
-    const deleteBookStorage = data.find(({ title }) => title === deleteBookName);
-    console.log(deleteBookStorage);
+  if (!e.target.classList.contains(`icon-delete-button`)) {
+    // console.log(`no delete`);
+    return;
+  }
+  // console.log(` delete`);
+  const deleteBook = e.target.closest(`.shopping-list-card`);
+  // console.log(deleteBook);
+  const deleteBookName = deleteBook.dataset.title;
+  // console.log(deleteBookName);
+  let data = getDataLocalStorage();
+  // console.log(data);
 
-    const indexDeleteBook = data.findIndex(el => el.title === deleteBookName);
-    console.log(indexDeleteBook);
+  const deleteBookStorage = data.find(({ title }) => title === deleteBookName);
+  console.log(deleteBookStorage);
 
-    const newArray = data.splice(indexDeleteBook, 1);
+  const indexDeleteBook = data.findIndex(el => el.title === deleteBookName);
+  console.log(indexDeleteBook);
 
-    console.log(data);
+  const newArray = data.splice(indexDeleteBook, 1);
 
-    dataChangeLocalstorage(`project`, data);
+  console.log(data);
 
-    createMarkup(data);
+  dataChangeLocalstorage(`project`, data);
 
-}  
-
-
+  createMarkup(data);
+}
