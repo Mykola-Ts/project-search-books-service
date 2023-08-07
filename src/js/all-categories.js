24c8651b0ebeb8a7a1e742c5dff93876ddc9cb7d
Marini23 <marina.smile1m@gmail.com>
2023-08-07 17:37:40 +0200

    shoppind-list-fix-01

--- src/js/all-categories.js
+++ src/js/all-categories.js
@@ -1,150 +1,150 @@
 // All categories
-const API_URL = 'https://books-backend.p.goit.global/books';
-const defaultCategory = 'all categories';
+// const API_URL = 'https://books-backend.p.goit.global/books';
+// const defaultCategory = 'all categories';
 
-async function fetchData(endpoint) {
-  try {
-    const response = await fetch(`${API_URL}${endpoint}`);
-    if (!response.ok) throw new Error(response.statusText);
-    return await response.json();
-  } catch (error) {
-    console.error('Error request data:', error);
-  }
-}
-async function fetchCategoryData(categoryName) {
-  return await fetchData(`/category?category=${categoryName}`);
-}
+// async function fetchData(endpoint) {
+//   try {
+//     const response = await fetch(`${API_URL}${endpoint}`);
+//     if (!response.ok) throw new Error(response.statusText);
+//     return await response.json();
+//   } catch (error) {
+//     console.error('Error request data:', error);
+//   }
+// }
+// async function fetchCategoryData(categoryName) {
+//   return await fetchData(`/category?category=${categoryName}`);
+// }
 
-// елементи
-function createCategoryElement(list_name) {
-  const category = document.createElement('div');
-  category.className = 'category';
-  category.dataset.category = list_name;
-  category.textContent = list_name;
+// // елементи
+// function createCategoryElement(list_name) {
+//   const category = document.createElement('div');
+//   category.className = 'category';
+//   category.dataset.category = list_name;
+//   category.textContent = list_name;
 
-  category.addEventListener('click', () => {
-    document.querySelectorAll('.category').forEach(cat => {
-      cat.classList.remove('selected-category');
-    });
-    category.classList.add('selected-category');
+//   category.addEventListener('click', () => {
+//     document.querySelectorAll('.category').forEach(cat => {
+//       cat.classList.remove('selected-category');
+//     });
+//     category.classList.add('selected-category');
 
-    if (list_name !== defaultCategory) {
-      updateMainTitle(list_name);
-      displayBooksByCategory(list_name);
-    } else {
-      updateMainTitle('Best Sellers Books');
-      displayAllBlock();
-    }
-  });
+//     if (list_name !== defaultCategory) {
+//       updateMainTitle(list_name);
+//       displayBooksByCategory(list_name);
+//     } else {
+//       updateMainTitle('Best Sellers Books');
+//       displayAllBlock();
+//     }
+//   });
 
-  //  "all categories" за замовченням
-  if (list_name === defaultCategory) {
-    category.click();
-  }
+//   //  "all categories" за замовченням
+//   if (list_name === defaultCategory) {
+//     category.click();
+//   }
 
-  return category;
-}
+//   return category;
+// }
 
-function createBookCard({ book_image, title, author }) {
-  const bookCard = document.createElement('div');
-  bookCard.className = 'book';
-  bookCard.innerHTML = `
-    <img src="${book_image}" alt="${title}">
-    <h3 class="book-title">${title}</h3>
-    <p class="book-author">by ${author}</p>
-    `;
-  return bookCard;
-}
+// function createBookCard({ book_image, title, author }) {
+//   const bookCard = document.createElement('div');
+//   bookCard.className = 'book';
+//   bookCard.innerHTML = `
+//     <img src="${book_image}" alt="${title}">
+//     <h3 class="book-title">${title}</h3>
+//     <p class="book-author">by ${author}</p>
+//     `;
+//   return bookCard;
+// }
 
-function createCategoryBooksBlock(categoryName, books) {
-  const block = document.createElement('div');
-  block.className = 'category-books';
+// function createCategoryBooksBlock(categoryName, books) {
+//   const block = document.createElement('div');
+//   block.className = 'category-books';
 
-  const categoryHeader = document.createElement('div');
-  categoryHeader.className = 'category-header';
+//   const categoryHeader = document.createElement('div');
+//   categoryHeader.className = 'category-header';
 
-  const headerText =
-    categoryName === defaultCategory ? 'Best Sellers Books' : categoryName;
-  categoryHeader.innerHTML = `<h2 class="category-title">${headerText}</h2>`;
+//   const headerText =
+//     categoryName === defaultCategory ? 'Best Sellers Books' : categoryName;
+//   categoryHeader.innerHTML = `<h2 class="category-title">${headerText}</h2>`;
 
-  const categoryContent = document.createElement('div');
-  categoryContent.className = 'category-content';
-  books.forEach(book => categoryContent.appendChild(createBookCard(book)));
+//   const categoryContent = document.createElement('div');
+//   categoryContent.className = 'category-content';
+//   books.forEach(book => categoryContent.appendChild(createBookCard(book)));
 
-  const seeMoreButton = document.createElement('button');
-  seeMoreButton.className = 'see-more';
-  seeMoreButton.textContent = 'SEE MORE';
-  seeMoreButton.addEventListener('click', () =>
-    displayBooksByCategory(categoryName)
-  );
+//   const seeMoreButton = document.createElement('button');
+//   seeMoreButton.className = 'see-more';
+//   seeMoreButton.textContent = 'SEE MORE';
+//   seeMoreButton.addEventListener('click', () =>
+//     displayBooksByCategory(categoryName)
+//   );
 
-  block.appendChild(categoryHeader);
-  block.appendChild(categoryContent);
-  block.appendChild(seeMoreButton);
-  return block;
-}
+//   block.appendChild(categoryHeader);
+//   block.appendChild(categoryContent);
+//   block.appendChild(seeMoreButton);
+//   return block;
+// }
 
-// Відображення данных
-async function displayBooksByCategory(category) {
-  updateMainTitle(category);
-  const booksDiv = document.getElementById('books');
-  booksDiv.innerHTML = 'Loading...';
-  const books = await fetchCategoryData(category);
-  booksDiv.innerHTML = '';
-  books.forEach(book => booksDiv.appendChild(createBookCard(book)));
-}
+// // Відображення данных
+// async function displayBooksByCategory(category) {
+//   updateMainTitle(category);
+//   const booksDiv = document.getElementById('books');
+//   booksDiv.innerHTML = 'Loading...';
+//   const books = await fetchCategoryData(category);
+//   booksDiv.innerHTML = '';
+//   books.forEach(book => booksDiv.appendChild(createBookCard(book)));
+// }
 
-async function displayAllBlock() {
-  updateMainTitle(defaultCategory);
-  const categories = document.querySelectorAll('.category');
-  const booksDiv = document.getElementById('books');
-  booksDiv.innerHTML = '';
-  for (const category of categories) {
-    const categoryName = category.textContent;
-    if (categoryName === defaultCategory) continue;
-    const books = await fetchData(`/category?category=${categoryName}`);
-    booksDiv.appendChild(
-      createCategoryBooksBlock(categoryName, books.slice(0, 5))
-    );
-  }
-}
+// async function displayAllBlock() {
+//   updateMainTitle(defaultCategory);
+//   const categories = document.querySelectorAll('.category');
+//   const booksDiv = document.getElementById('books');
+//   booksDiv.innerHTML = '';
+//   for (const category of categories) {
+//     const categoryName = category.textContent;
+//     if (categoryName === defaultCategory) continue;
+//     const books = await fetchData(`/category?category=${categoryName}`);
+//     booksDiv.appendChild(
+//       createCategoryBooksBlock(categoryName, books.slice(0, 5))
+//     );
+//   }
+// }
 
-function updateMainTitle(categoryName) {
-  const titleElement = document.querySelector('.title-category');
-  let words =
-    categoryName === defaultCategory ? 'Best Sellers Books' : categoryName;
-  words = words.split(' ');
-  const lastWord = words.pop();
-  titleElement.innerHTML = `${words.join(
-    ' '
-  )} <span class="title-category-span">${lastWord}</span>`;
-}
+// function updateMainTitle(categoryName) {
+//   const titleElement = document.querySelector('.title-category');
+//   let words =
+//     categoryName === defaultCategory ? 'Best Sellers Books' : categoryName;
+//   words = words.split(' ');
+//   const lastWord = words.pop();
+//   titleElement.innerHTML = `${words.join(
+//     ' '
+//   )} <span class="title-category-span">${lastWord}</span>`;
+// }
 
-async function fetchAndDisplayCategories() {
-  const categoriesData = await fetchData('/top-books');
-  const categoryList = document.getElementById('categoryList');
-  categoryList.appendChild(createCategoryElement(defaultCategory));
-  categoriesData.forEach(({ list_name }) =>
-    categoryList.appendChild(createCategoryElement(list_name))
-  );
-  displayAllBlock();
-}
+// async function fetchAndDisplayCategories() {
+//   const categoriesData = await fetchData('/top-books');
+//   const categoryList = document.querySelector('#categoryList');
+//   categoryList.appendChild(createCategoryElement(defaultCategory));
+//   categoriesData.forEach(({ list_name }) =>
+//     categoryList.appendChild(createCategoryElement(list_name))
+//   );
+//   displayAllBlock();
+// }
 
-document.addEventListener('DOMContentLoaded', fetchAndDisplayCategories);
+// document.addEventListener('DOMContentLoaded', fetchAndDisplayCategories);
 
-export { fetchCategoryData };
+// export { fetchCategoryData };
 
-// як вам забрати данні по категорії
-// import export { fetchCategoryData };
-// <!--async function getAndProcessData() {-->
-//     <!--try {-->
-//     <!--const desiredCategory = "your_category_name"; //  ваша категорія-->
-//     <!--const data = await fetchCategoryData(desiredCategory);-->
-//     <!--console.log(data);-->
-//     <!--// -->
-//     <!--} catch (error) {-->
-//     <!--console.error("error:", error);-->
-//     <!--}-->
-//     <!--}-->
+// // як вам забрати данні по категорії
+// // import export { fetchCategoryData };
+// // <!--async function getAndProcessData() {-->
+// //     <!--try {-->
+// //     <!--const desiredCategory = "your_category_name"; //  ваша категорія-->
+// //     <!--const data = await fetchCategoryData(desiredCategory);-->
+// //     <!--console.log(data);-->
+// //     <!--// -->
+// //     <!--} catch (error) {-->
+// //     <!--console.error("error:", error);-->
+// //     <!--}-->
+// //     <!--}-->
 
-//     <!--getAndProcessData();-->
+// //     <!--getAndProcessData();-->
--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,38 +1,30 @@
 // SHOPPING LIST
 const shoppinglist = document.querySelector(`.shopping-list`);
-const btnFromHeader = document.querySelector(`.btn-to-list`);
-const blockSupportUkraine = document.querySelector(`.shopping-list-support-ukraine`);
 const shoppinglistContainer = document.querySelector(`.shopping-list-container`);
+const clickToShoppingList = document.querySelector(`.header-nav-container`);
+const btnDeletebook = document.querySelector(`.shopping-list`);
 
 // Для тестирования - получение и загрузка данных в localStorage.
 // После загрузки данных , нужно закоментировать.
-function getBooks() {
-    return fetch(`https://books-backend.p.goit.global/books/category?category=Series Books`)
-        .then((response) => {
-        if (!response.ok) {
-        throw new Error(response.statusText)
-            }
-            const dataBook = response.json();
-        return dataBook;
-        })
-};
-getBooks();
-function addToLocalstorage() {
-    getBooks().then(dataBook => {
-        let book = dataBook;
-        dataChangeLocalstorage(`project`, book);
-    })
-}
-addToLocalstorage();
+// function getBooks() {
+//     return fetch(`https://books-backend.p.goit.global/books/category?category=Series Books`)
+//         .then((response) => {
+//         if (!response.ok) {
+//         throw new Error(response.statusText)
+//         }
+//             const dataBook = response.json();
+//         return dataBook;
+//         })
+// };
+// getBooks();
+// function addToLocalstorage() {
+//     getBooks().then(dataBook => {
+//         let book = dataBook;
+//         dataChangeLocalstorage(`project`, book);
+//     })
+// }
+// addToLocalstorage();
 
-function dataChangeLocalstorage(key, value) {
-    try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-    } catch (error) {
-    console.error("Set state error: ", error.message);
-    }
-};
 
 
 // localStorage.clear();
@@ -41,14 +33,56 @@ function dataChangeLocalstorage(key, value) {
 
 // Рабочий код
 
-btnFromHeader.addEventListener(`click`, onCreatePageShoppingList);
 
-function onCreatePageShoppingList(e) {
+clickToShoppingList.addEventListener(`click`, createShoppingList);
+btnDeletebook.addEventListener(`click`, onDeleteBook);
+
+function createShoppingList(e) {
     e.preventDefault();
+    console.log(e.target);
+    if (!e.target.classList.contains(`shoppinglist`)) {
+        console.log(`no delete`);
+        return;
+    }
+
+    // const page = e.target.closest(`.header-nav-list`);
+    // console.log(page);
     let data = getDataLocalStorage();
     createMarkup(data);
+    
 }
 
+function onDeleteBook(e) {
+    e.preventDefault();
+    // console.log(e.target);
+    
+    if (!e.target.classList.contains(`icon-delete-button`)) {
+        // console.log(`no delete`);
+        return;
+    }
+    // console.log(` delete`);
+    const deleteBook = e.target.closest(`.shopping-list-card`);
+    // console.log(deleteBook);
+    const deleteBookName = deleteBook.dataset.title;
+    // console.log(deleteBookName);
+    let data = getDataLocalStorage();
+    // console.log(data);
+
+    const deleteBookStorage = data.find(({ title }) => title === deleteBookName);
+    console.log(deleteBookStorage);
+
+    const indexDeleteBook = data.findIndex(el => el.title === deleteBookName);
+    console.log(indexDeleteBook);
+
+    const newArray = data.splice(indexDeleteBook, 1);
+
+    console.log(data);
+
+    dataChangeLocalstorage(`project`, data);
+
+    createMarkup(data);
+
+}  
 
 function getDataLocalStorage() {
     const savedData = localStorage.getItem(`project`);
@@ -59,7 +93,7 @@ function getDataLocalStorage() {
 
 function createMarkup(data) {
 
-    blockSupportUkraine.innerHTML = '<div class="shopping-list-support-ukraine"> Support UKraine</div>'
+    // blockSupportUkraine.innerHTML = '<div class="shopping-list-support-ukraine"> Support UKraine</div>'
     shoppinglistContainer.innerHTML = `
     <h2 class="shopping-list-title-part1 ">Shopping <span class="shopping-list-title-part2">List</span></h2>
     `
@@ -68,7 +102,6 @@ function createMarkup(data) {
 
     if (data.length != 0) {
         shoppinglistContainer.innerHTML += data.map(el => `
-    
     <div class="shopping-list-card" data-title="${el.title}">
     
     <img class="shopping-list-card-img" src="${el.book_image}" alt="book image" />
@@ -127,42 +160,12 @@ function createMarkup(data) {
     }
     
 
-// doMarkup();
-
-const btnDeletebook = document.querySelector(`.shopping-list`);
-btnDeletebook.addEventListener(`click`, onDeleteBook);
-
-
-function onDeleteBook(e) {
-    e.preventDefault();
-    console.log(e.target);
-    
-    if (!e.target.classList.contains(`icon-delete-button`)) {
-        // console.log(`no delete`);
-        return;
+function dataChangeLocalstorage(key, value) {
+    try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+    } catch (error) {
+    console.error("Set state error: ", error.message);
     }
-    // console.log(` delete`);
-    const deleteBook = e.target.closest(`.shopping-list-card`);
-    // console.log(deleteBook);
-    const deleteBookName = deleteBook.dataset.title;
-    // console.log(deleteBookName);
-    let data = getDataLocalStorage();
-    // console.log(data);
-
-    const deleteBookStorage = data.find(({ title }) => title === deleteBookName);
-    console.log(deleteBookStorage);
-
-    const indexDeleteBook = data.findIndex(el => el.title === deleteBookName);
-    console.log(indexDeleteBook);
-
-    const newArray = data.splice(indexDeleteBook, 1);
-
-    console.log(data);
-
-    dataChangeLocalstorage(`project`, data);
-
-    createMarkup(data);
-
-}  
-
+};
 
