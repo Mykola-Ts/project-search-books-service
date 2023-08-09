d1f161bc2732f9f1855df9d23165bf105955ec80
Irina Antonishina <111454571+IrinaAntonishina@users.noreply.github.com>
2023-08-09 23:28:29 +0300

    redesigned mobile menu
    переробила мобільне меню

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,22 +1,31 @@
 // MOBILE MENU
 const refs = {
-    btnMobMenu: document.querySelector(".mobile-menu-btn"),
-    containerMobMenu: document.querySelector('.mobile-menu_container'),
+    burger: document.querySelector(".js-open-menu"),
+    containerMobMenu: document.querySelector('.header-nav-menu'),
     body:document.querySelector('body'),
+    signUpBtn:document.querySelector('button[data-login-button]'),
+    authBackdrop:document.querySelector('div[data-modal]'),
 }
 
-refs.btnMobMenu.addEventListener('click', openAndCloseMenu)
+refs.burger.addEventListener('click', openAndCloseMenu)
+refs.signUpBtn.addEventListener('click', hiddenMobileMenu)
 
 function openAndCloseMenu (evt) {
-    const expanded = refs.btnMobMenu.getAttribute('aria-expanded') === 'true'|| false 
-    refs.btnMobMenu.classList.toggle('is-open')
-    refs.btnMobMenu.setAttribute('aria-expanded', !expanded)
+    const expanded = refs.burger.getAttribute('aria-expanded') === 'true'|| false 
+    refs.burger.classList.toggle('is-open')
+    refs.burger.setAttribute('aria-expanded', !expanded)
 
     refs.containerMobMenu.classList.toggle('is-open')
+    refs.containerMobMenu.classList.remove('hidden')
 
     if(refs.containerMobMenu.classList.contains('is-open')){
         refs.body.style.overflow = 'hidden'
     }else{
         refs.body.style.overflow = 'visible'
     }
+}
+
+function hiddenMobileMenu () {
+    refs.containerMobMenu.classList.add('hidden')
+    
 }
