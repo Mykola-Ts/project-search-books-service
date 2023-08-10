792bec0dfda4fce9e1025acff6467a4ef66eb3e1
Marini23 <marina.smile1m@gmail.com>
2023-08-10 20:59:12 +0200

    shopping-list-05-fix4

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -97,12 +97,12 @@ function createMarkup(data) {
         </li>
         <li class="shop-item">
             <a class="shop-link" href="${el.buyLinks[1].url}" target="_blank" rel="noopener noreferrer nofollow">
-            <img class="shopping-list-applebook-img" src="${appleBookIcon}" alt="logo-aapplebook " width="16" height="16" />
+            <img class="shopping-list-applebook-img" src="${appleBookIcon}" alt="logo-applebook " width="16" height="16" />
             </a>
         </li>
         <li class="shop-item">
             <a class="shop-link" href="${el.buyLinks[2].url}" target="_blank" rel="noopener noreferrer nofollow">
-            <img class="shopping-list-applebook-img" src="${bookShopIcon}" alt="logo-aapplebook " width="16"
+            <img class="shopping-list-bookshop-img" src="${bookShopIcon}" alt="logo-bookshop " width="16"
   height="16" />
                 
             </a>
